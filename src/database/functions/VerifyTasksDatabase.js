4ab29c6293704720fb6119dc33917c591f30b7dc
heuderdev <heuderdev@gmail.com>
2023-09-09 08:25:09 -0300

    horario atual

--- src/database/functions/VerifyTasksDatabase.js
+++ src/database/functions/VerifyTasksDatabase.js
@@ -8,7 +8,7 @@ class VerifyTasksDatabase {
     try {
       const [tasks] = await poll.query(
         "SELECT  DISTINCT  contributions.id as contributions_id,contributions.phone AS contributions_phone, contributions.name AS contributions_name,  tasks.id as tasks_id,tasks.name AS tasks_name, schedules.time AS schedules_time, contributions.name AS contributions_name	from tasks   inner join schedules_tasks on schedules_tasks.task_id = tasks.id inner join schedules on schedules.id = schedules_tasks.schedule_id   inner join contributors_tasks on  contributors_tasks.task_id = tasks.id   inner join contributions on contributions.id = contributors_tasks.contributor_id WHERE schedules.time = ?",
-        ["00:00"]
+        [HoraAtual]
       );
       return tasks;
     } catch (error) {
